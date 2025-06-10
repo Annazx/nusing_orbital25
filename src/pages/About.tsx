e74be9db5ed76ea2d5f3f17f827e8b7f29aa1fc7
Gan Jiayi <ganjy0614@gmail.com>
2025-06-10 15:22:46 +0800

    added another question mark

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <>
       <h1>About</h1>
-      <h3>What is NUSing My Brain?</h3>
+      <h3>What is NUSing My Brain??</h3>
       <h3>Steps to Get Started</h3>
       <h3>Privacy Policy</h3>
     </>
