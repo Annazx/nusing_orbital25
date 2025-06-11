effa68ae8898c2f9a7ad6fae249c2813b96758e9
Anna Zoe Tan Xi <e1356946@u.nus.edu>
2025-06-11 16:14:34 +0800

    added another question mark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Providers from "@/components;
+import Providers from "@/components/Providers";
 import TopNav from "@/components/navbar/TopNav";
 
 export const metadata: Metadata = {
