f5d1e0554492b9d55eef6836b0f9adedf3ffa24f
Anna Zoe Tan Xi <e1356946@u.nus.edu>
2025-06-11 12:55:13 +0800

    redoing

---
+++ nusing/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Providers from "@/components/Providers";
+import TopNav from "@/components/navbar/TopNav";
+
+export const metadata: Metadata = {
+  title: "NUSing My Brain",
+  description: "Peer-tutoring website built for NUS students by NUS students",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <TopNav />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ nusing/src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Button, Link } from "@heroui/react";
+import { GoSmiley } from "react-icons/go"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">hey</h1>
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<GoSmiley size={20} />}
+      >
+        Click me
+      </Button>
+    </div>
+  );
+}
