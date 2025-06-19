f27a823ad4b18320e52c89678aa683a0e0855ed8
Anna Zoe Tan Xi <e1356946@u.nus.edu>
2025-06-19 16:40:21 +0800

    changed navbar color

--- nusing_template/components/navbar.tsx
+++ nusing_template/components/navbar.tsx
@@ -49,7 +49,7 @@ export const Navbar = () => {
   );
 
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
+    <HeroUINavbar maxWidth="xl" position="sticky" className="bg-[oklch(66.64%_0.0437_194.23)]">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
