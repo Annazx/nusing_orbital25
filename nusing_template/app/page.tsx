4c15dca2c9c473d99e9cd8fc6014a5dafe785bc5
Gan Jiayi <ganjy0614@gmail.com>
2025-06-19 16:18:27 +0800

    edited home page

--- nusing_template/app/page.tsx
+++ nusing_template/app/page.tsx
@@ -11,14 +11,13 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title()}>Welcome to&nbsp;</span>
         <br />
-        <span className={title()}>
-          websites regardless of your design experience.
+        <span className={title({ color: "violet" })}>
+          NUSing My Brain
         </span>
         <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+          Are you a student or peer tutor?
         </div>
       </div>
 
@@ -32,15 +31,14 @@ export default function Home() {
           })}
           href={siteConfig.links.docs}
         >
-          Documentation
+          Student
         </Link>
         <Link
           isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          className={buttonStyles({ variant: "shadow", radius: "full" })}
           href={siteConfig.links.github}
         >
-          <GithubIcon size={20} />
-          GitHub
+          Peer tutor
         </Link>
       </div>
 
